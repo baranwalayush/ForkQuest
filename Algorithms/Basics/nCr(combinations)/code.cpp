fdecaa109d4f4aa7e26dd33e600e43c10aeaf281
AkashYadav-27 <akashy54_soe@jnu.ac.in>
2025-11-07 10:29:40 +0530

    Implement combination function in nCr code
    Added combination function to calculate nCr.

--- Algorithms/Basics/nCr(combinations)/code.cpp
+++ Algorithms/Basics/nCr(combinations)/code.cpp
@@ -4,14 +4,23 @@ using namespace std;
 long long factorial(int n) {
     long long fact = 1;
     for (int i = 1; i <= n; i++) {
-        fact = i;
+        fact *= i;
     }
     return fact;
 }
 
+long long comb(int n, int r){
+    long long result = factorial(n)/(factorial(n-r) * factorial(r));
+
+    return result;
+}
+
+
 int main() {
-    int n;
+    int n, r;
     cin >> n;
+    cin >> r;
     cout << factorial(n) << endl;
+    cout << comb(n, r);
     return 0;
 }
