4aab1a49dc4f61b4e554d01835bda62bc634c9bd
kartikrastogi18 <kartikrastogi4544@gmail.com>
2025-11-07 11:29:12 +0530

    solver search in rotated array

--- Algorithms/Searching/BS_on_rotated_search/code.cpp
+++ Algorithms/Searching/BS_on_rotated_search/code.cpp
@@ -1,15 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int searchInRotatedArray(vector<int>& nums, int target) {
-    int left = 0, right = nums.size() - 1;
+int searchInRotatedArray(vector<int>& nums, int target) 
+    {
+        int n=nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-    
+        if (nums[mid] == target)
+            return mid;
 
-
-    
-    return -1;
+     
+        if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+       
+        else {
+            if (target > nums[mid] && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;  // Not found
 }
+
+
 
 int main() {
     int n, target;
