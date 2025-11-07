c4b30cbaa161a0d5a8fb9011af1f9c26d735f0fd
Lucky Gehlot <luckygehlot1400@gmail.com>
2025-11-07 10:28:44 +0530

    Team 07 implemented bfs logic

--- Algorithms/Tree/Traversal/bfs.cpp
+++ Algorithms/Tree/Traversal/bfs.cpp
@@ -11,8 +11,16 @@ struct TreeNode {
     TreeNode(int value, TreeNode* l, TreeNode* r): val(value), left(l), right(r) {}
 };
 
-void bfs(TreeNode* root) {
+void bfs(TreeNode* root,queue<TreeNode*> &q) {
     // BFS functions
+    q.push(root);
+    while(!q.empty()){
+        TreeNode* node = q.front();
+        cout << node -> val << " " << endl;
+        q.pop();
+        if(node -> left) q.push(node -> left);
+        if(node -> right) q.push(node -> right);
+    }
 }
 
 int main() {
@@ -23,7 +31,7 @@ int main() {
          / \   \
         4   5   6
     */
-
+    queue<TreeNode*> q;
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
@@ -32,7 +40,7 @@ int main() {
     root->right->right = new TreeNode(6);
 
     cout << "BFS Traversal: ";
-    bfs(root);
+    bfs(root,q);
     cout << endl;
 
     return 0;
