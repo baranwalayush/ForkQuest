9abbc3a20bb1f27919961866451ae988c90df783
Rahul3106 <Rahul44_soe@jnu.ac.in>
2025-11-07 11:40:55 +0530

    Implementing  map and set By team 11 Issue 37

--- Algorithms/Associative_Containers/Map/Implementation_using_vector/implementation.cpp
+++ Algorithms/Associative_Containers/Map/Implementation_using_vector/implementation.cpp
@@ -5,75 +5,96 @@ using namespace std;
 class MyMap{
 private:
     // private variables/functions
+    vector<pair<int, int>> data;
 
 public:
 
     // Constructor
     MyMap(){
-
-
+        data.clear();
     }
 
 
     // insert function
     void insert(int key, int value){
-
+        for(auto &kv : data){
+            if(kv.first == key){
+                kv.second = value;
+                return;
+            }
+        }
+        data.push_back({key, value});
 
     }
 
 
     // erase function
     void erase(int key){
-
+        for(auto it = data.begin(); it != data.end(); ++it){
+            if(it->first == key){
+                data.erase(it);
+                return;
+            }
+        }
 
     }
 
 
     // find function
     bool find(int key){
-
+        for(auto &kv : data){
+            if(kv.first == key){
+                return true;
+            }
+        }
+        return false;
 
     }
 
 
     // get function
     int get(int key){
-
+        for(auto &kv : data){
+            if(kv.first == key){
+                return kv.second;
+            }
+        }
+        throw runtime_error("Key not found");
 
     }
 
 
     // size function
     int size(){
-
+        return data.size();
 
     }
 
 
     // empty function
     bool empty(){
-
+        return data.empty();
 
     }
 
 
     // clear function
     void clear(){
-
-        
+        data.clear();
     }
 
 
     // display function
     void display(){
-
-
+        for(auto &kv : data){
+            cout << "Key: " << kv.first << ", Value: " << kv.second << endl;
+        }
     }
 };
 
 
 int main(){
     // Add some examples for every function mentioned in MyMap class here
-
+    MyMap myMap;    
     return 0;
 }
