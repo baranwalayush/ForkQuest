c204a9872b87b94928df8dee58fd7471ebc8ab0f
SarveshShahane <shahan77_soe@jnu.ac.in>
2025-11-07 11:41:47 +0530

    fixed implementation

--- Algorithms/Searching/BS/code.cpp
+++ Algorithms/Searching/BS/code.cpp
@@ -1,18 +1,20 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
 using namespace std;
 
-int binarySearch(vector<int> &arr, x) {
-    int left = 0, right = arr.size();
+int binarySearch(vector<int> &arr,int  x) {
+    int left = 0, right = arr.size()-1;
     
     while (left <= right) {
         int mid = left + (right - left) / 2; 
         
-        if (arr[mid] = x)
+        if (arr[mid]== x)
             return mid;           
         else if (arr[mid] < x)
             left = mid + 1;       
         else
-            right == mid - 1;      
+            right = mid - 1;      
     }
     return -1; 
 }
