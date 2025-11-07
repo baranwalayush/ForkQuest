ff502d3aaa251eafd0307c1f7a712d77bc3a61c2
SarveshShahane <shahan77_soe@jnu.ac.in>
2025-11-07 10:47:49 +0530

    fixed nCr logic

--- Algorithms/Basics/nCr(combinations)/code.cpp
+++ Algorithms/Basics/nCr(combinations)/code.cpp
@@ -1,17 +1,22 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 long long factorial(int n) {
     long long fact = 1;
     for (int i = 1; i <= n; i++) {
-        fact = i;
+        fact =fact* (long long)i;
     }
     return fact;
 }
 
 int main() {
     int n;
+    int r;
     cin >> n;
-    cout << factorial(n) << endl;
+    cin>>r;
+    // cout << factorial(n) << endl;
+    long long combination= factorial(n)/(factorial(r)*factorial(n-r));
+    cout<<combination;
     return 0;
 }
