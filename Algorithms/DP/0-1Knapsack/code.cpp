b3f4d4acf1a5dff102f6c879934f021d15e4ac42
kartikrastogi18 <kartikrastogi4544@gmail.com>
2025-11-07 11:39:22 +0530

    solved Knapsack

--- Algorithms/DP/0-1Knapsack/code.cpp
+++ Algorithms/DP/0-1Knapsack/code.cpp
@@ -1,22 +1,31 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 
-int knapsack(int n, int W, vector<int>& wt, vector<int>& val) {
-    
-    
-    
+#include <stdio.h>
 
-    
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapsack(int W, int wt[], int val[], int n) {
+    int i, w;
+    int K[n + 1][W + 1];
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
 }
 
 int main() {
-    int n, W;
-    cin >> n >> W;
-    vector<int> wt(n), val(n);
-    for (int i = 0; i < n; i++) cin >> wt[i];
-    for (int i = 0; i < n; i++) cin >> val[i];
-
-    cout << knapsack(n, W, wt, val) << endl;
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("Maximum value in Knapsack = %d\n", knapsack(W, wt, val, n));
     return 0;
 }
