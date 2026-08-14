e8e2e2cd5339de148fe5c8387b1d482a50d4958f
ayush-demo <ayush273304@gmail.com>
2026-08-14 13:50:23 +0530

    Fix bug in prime number and optimize

--- Algorithms/Basics/prime_check/code.cpp
+++ Algorithms/Basics/prime_check/code.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 bool isPrime(int n) {
-    if (n <= 1)
-        return true; 
+    if (n <=1 )
+        return false; 
 
-    for (int i = 2; i < n; i++) { 
+    for (int i = 2; i*i < n; i++) { 
         if (n % i == 0)
             return false;
     }
