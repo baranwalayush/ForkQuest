2e5f133cceda6dd2483477f36cba92bac2e8a87c
ayush-demo <ayush273304@gmail.com>
2026-08-16 07:59:29 +0530

    improve loop and add another check point for prime number=2

--- Algorithms/Basics/prime_check/code.cpp
+++ Algorithms/Basics/prime_check/code.cpp
@@ -4,8 +4,10 @@ using namespace std;
 bool isPrime(int n) {
     if (n <=1 )
         return false; 
-
-    for (int i = 2; i*i < n; i++) { 
+    if( n==2) {
+        return true;
+    }
+    for (int i = 2; i*i <= n; i++) { 
         if (n % i == 0)
             return false;
     }
