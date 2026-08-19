d5fab3cb9b390efab1b6fa8c7e46e795a34be6c0
baranwalayush <ayush6262kr@gmail.com>
2026-08-19 23:28:02 +0530

    remove separate prime check for 2

--- Algorithms/Basics/prime_check/code.cpp
+++ Algorithms/Basics/prime_check/code.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 bool isPrime(int n) {
-    if (n <=1 ){
+    if (n <= 1 ){
         return false;
     }
-      
-    if( n==2) {
-        return true;
-    }
+   
     for (int i = 2; i*i <= n; i++) { 
         if (n % i == 0)
             return false;
     }
+    
     return true;
 }
 
